0b7efb8ba989b3c9706903e3598c8b16e2476f44
junhan-ye <JASMINEYEAH54@GMAIL.COM>
2026-01-31 21:02:58 +0800

    Update projects-data.js

--- projects-data.js
+++ projects-data.js
@@ -5,8 +5,8 @@
 const PROJECTS = [
   { title: 'OLD MYTHOLOGY, NEW CONNECTIONS ', section: 'DH', description: 'An Immersive 3D Digital Storytelling Approach to Global Creation Myths.', 
    link: 'https://drive.google.com/file/d/1j90StDAVqq13C7cbj9VenTpzTnjQul68/view?usp=drive_link', year: '2024-2025' },
-  { title: 'Generative AI, Embodied Media', section: 'CriticalAI', description: 'A Critical Thinking on Generative AI Art.', 
-   link: 'https://drive.google.com/file/d/1yx7gxIVisNrkavw50MOXD_0oR1j-b1Cn/view?usp=drive_link', year: '2023' },
+  { title: 'Meta Data, Structured Analysis', section: 'DH', description: 'MIGRATION MAP OF CHINESE MINORITY POPULATION IN A HISTORICAL PERIOD.', 
+   link: 'https://drive.google.com/file/d/1sCPmJqQ3RnI50jMSAqTd-LZJ-D0eVmmJ/view?usp=sharing', year: '2023' },
   { title: 'NEW VISIONS, EXTENDED SENSES', section: 'Tech', description: 'A Semi-supervised 3D Hand Pose Estimation CNN.', 
    link: 'https://drive.google.com/file/d/1TESInImLI-BlSkogc9qGEuIpJPJI5Qjs/view?usp=drive_link', year: '2022-2023' },
   { title: 'EXTENDED REALITIES, BETTER INHERITANCE', section: 'DH', description: 'The Application of AI and Digitalization in Culture Inheritance.', 
