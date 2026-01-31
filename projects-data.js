876e6dccf87ef159c86e70553adb4d94eea289b8
junhan-ye <JASMINEYEAH54@GMAIL.COM>
2026-01-31 17:38:33 +0800

    Update projects-data.js

--- projects-data.js
+++ projects-data.js
@@ -5,10 +5,10 @@
 const PROJECTS = [
   { title: 'OLD MYTHOLOGY, NEW CONNECTIONS ', section: 'DH', description: 'An Immersive 3D Digital Storytelling Approach to Global Creation Myths.', 
    link: 'https://drive.google.com/file/d/1j90StDAVqq13C7cbj9VenTpzTnjQul68/view?usp=drive_link', year: '2024-2025' },
-  { title: 'NEW VISIONS, EXTENDED SENSES', section: 'Tech', description: 'A Semi-supervised 3D Hand Pose Estimation CNN.', 
-   link: 'https://drive.google.com/file/d/1TESInImLI-BlSkogc9qGEuIpJPJI5Qjs/view?usp=drive_link', year: '2022-2023' },
-  { title: 'EXTENDED REALITIES, BETTER INHERITANCES', section: 'DH', description: 'The Application of AI and Digitalization in Culture Inheritance.', 
-   link: 'https://drive.google.com/file/d/1sJr8lWtlXrdIalVqWVLm6r7kgQlUQyO0/view?usp=drive_link', year: '2022' },
   { title: 'Generative AI, Embodied Media', section: 'CriticalAI', description: 'A Critical Thinking on Generative AI Art.', 
    link: 'https://drive.google.com/file/d/1yx7gxIVisNrkavw50MOXD_0oR1j-b1Cn/view?usp=drive_link', year: '2023' },
+  { title: 'NEW VISIONS, EXTENDED SENSES', section: 'Tech', description: 'A Semi-supervised 3D Hand Pose Estimation CNN.', 
+   link: 'https://drive.google.com/file/d/1TESInImLI-BlSkogc9qGEuIpJPJI5Qjs/view?usp=drive_link', year: '2022-2023' },
+  { title: 'EXTENDED REALITIES, BETTER INHERITANCE', section: 'DH', description: 'The Application of AI and Digitalization in Culture Inheritance.', 
+   link: 'https://drive.google.com/file/d/1sJr8lWtlXrdIalVqWVLm6r7kgQlUQyO0/view?usp=drive_link', year: '2022' },
  ];
