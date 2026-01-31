64e2cec1c044930942d59bf634b9f1541473d573
junhan-ye <JASMINEYEAH54@GMAIL.COM>
2026-01-31 21:08:51 +0800

    Update projects-data.js

--- projects-data.js
+++ projects-data.js
@@ -5,7 +5,7 @@
 const PROJECTS = [
   { title: 'OLD MYTHOLOGY, NEW CONNECTIONS ', section: 'DH', description: 'An Immersive 3D Digital Storytelling Approach to Global Creation Myths.', 
    link: 'https://drive.google.com/file/d/1j90StDAVqq13C7cbj9VenTpzTnjQul68/view?usp=drive_link', year: '2024-2025' },
-  { title: 'Meta Data, Structured Analysis', section: 'DH', description: 'MIGRATION MAP OF CHINESE MINORITY POPULATION IN A HISTORICAL PERIOD.', 
+  { title: 'Meta Data, Structured Analysis', section: 'DH', description: 'Migration Map Of Chinese Minority Population in a Historical Period.', 
    link: 'https://drive.google.com/file/d/1sCPmJqQ3RnI50jMSAqTd-LZJ-D0eVmmJ/view?usp=sharing', year: '2023' },
   { title: 'NEW VISIONS, EXTENDED SENSES', section: 'Tech', description: 'A Semi-supervised 3D Hand Pose Estimation CNN.', 
    link: 'https://drive.google.com/file/d/1TESInImLI-BlSkogc9qGEuIpJPJI5Qjs/view?usp=drive_link', year: '2022-2023' },
